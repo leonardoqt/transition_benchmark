00ed8570d75f080e27a68fc914133e44ad0257d2
Tian Qiu <tianq@sas.upenn.edu>
2022-04-22 13:41:25 -0400

    debug logm

--- benchmark_system.f90
+++ benchmark_system.f90
@@ -6,7 +6,7 @@ module benchmark_system
 	!
 	real(dp)  :: v0, dt
 	integer   :: nT, nstate
-	real(dp), allocatable :: U(:,:,:), E(:,:)
+	real(dp), allocatable :: U(:,:,:), E(:,:), T_rate(:,:,:)
 	real(dp), allocatable :: psi0(:), rho0(:,:) ! rho is used in real calculation
 	!
 	contains
@@ -37,10 +37,12 @@ module benchmark_system
 		nstate = nstate_in
 		nT = ceiling( (x_fin_in - x_ini_in) / (v0*dt) )
 		!
-		if ( allocated(U) ) deallocate(U)
-		if ( allocated(E) ) deallocate(E)
-		allocate( U(nstate,nstate,nT) )
-		allocate( E(nstate,nT) )
+		if ( allocated(U     ) ) deallocate(U     )
+		if ( allocated(E     ) ) deallocate(E     )
+		if ( allocated(T_rate) ) deallocate(T_rate)
+		allocate(      U(nstate,nstate,nT)   )
+		allocate(      E(nstate,nT)          )
+		allocate( T_rate(nstate,nstate,nT-1) )
 		if ( allocated(psi0) ) deallocate(psi0)
 		if ( allocated(rho0) ) deallocate(rho0)
 		allocate( psi0(nstate) )
@@ -86,6 +88,18 @@ module benchmark_system
 	end subroutine assign_rho
 	!
 	!
+	subroutine evo_npi()
+		!
+		implicit none
+		!
+		integer :: idt
+		!
+		do idt = 1, nT-1
+			T_rate(:,:,idt) = logm(matmul(transpose(U(:,:,idt)),U(:,:,idt+1)) )
+		enddo
+	end subroutine evo_npi
+	!
+	!
 	subroutine diag_real(H_in, vec, val)
 		!
 		implicit none
@@ -109,5 +123,65 @@ module benchmark_system
 		!
 	end subroutine
 	!
+	!
+	function logm(A_in)
+		!
+		implicit none
+		!
+		real(dp), dimension(:,:) :: A_in
+		real(dp), allocatable    :: A(:,:), logm(:,:)
+		!
+		! working variables
+		real(dp), allocatable :: wr(:),wi(:),vl(:,:),vr(:,:),work(:),zeros(:)
+		complex(dp), allocatable :: vec1(:,:), vec2(:,:), logw(:,:) ! A = vec1*w*vec2^\dagger
+		integer :: lwork, err_msg, incr, istate, jstate
+		!
+		!
+		lwork = (4*nstate)*3
+		!
+		allocate( A(nstate,nstate) )
+		allocate( logm(nstate,nstate) )
+		!
+		allocate( wr(nstate) )
+		allocate( wi(nstate) )
+		allocate( vl(nstate,nstate) )
+		allocate( vr(nstate,nstate) )
+		allocate( work(lwork) )
+		!
+		A = A_in
+		call dgeev('V','V',nstate,A,nstate,wr,wi,vl,nstate,vr,nstate,work,lwork,err_msg)
+		!
+		allocate( zeros(nstate) )
+		allocate( logw(nstate,nstate) )
+		allocate( vec1(nstate,nstate) )
+		allocate( vec2(nstate,nstate) )
+		logw = (0.d0, 0.d0)
+		incr = 1
+		do istate = 1,nstate
+			if ( abs(wi(istate)) < 1e-10 ) then
+				vec1(:,istate) = cmplx( vr(:,istate), zeros )
+				vec2(:,istate) = cmplx( vl(:,istate), zeros )
+			else
+				if (incr == 1) then
+				vec1(:,istate)   = cmplx( vr(:,istate), vr(:,istate+1) )
+				vec1(:,istate+1) = cmplx( vr(:,istate),-vr(:,istate+1) )
+				vec2(:,istate)   = cmplx( vl(:,istate),-vl(:,istate+1) ) ! conjugate is applied
+				vec2(:,istate+1) = cmplx( vl(:,istate), vl(:,istate+1) ) ! conjugate is applied
+				endif
+				incr = -incr
+			endif
+			logw(istate,istate) = cmplx( wr(istate), wi(istate) )
+		enddo
+		write(*,'(3(ES12.4,1X))') A_in
+		write(*,*) ''
+		write(*,'(3(ES12.4,1X))') wi
+		write(*,*) ''
+		write(*,'(3(ES12.4,1X))') dble( matmul(matmul(vec1,logw),transpose(vec2)) )
+		write(*,*) ''
+		write(*,*) ''
+		!
+		deallocate(wr,wi,vl,vr,work,zeros,A,vec1,vec2,logw)
+		!
+	end function logm
 	!
 end module benchmark_system
--- main.f90
+++ main.f90
@@ -1,6 +1,6 @@
 program test
 	!
-	use benchmark_system,  only: assign_model, assign_psi, U, E, rho0
+	use benchmark_system,  only: assign_model, assign_psi, U, E, rho0, evo_npi
 	use model_H,           only: sz1, sz2, H1, H2
 	use iso_fortran_env,   only: dp=> real64
 	!
@@ -10,11 +10,12 @@ program test
 	!
 	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d0,H1,sz1)
 	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d0,H2,sz2)
-	write(*,'(3(ES12.4,1X))') U
+	!write(*,'(3(ES12.4,1X))') U
 	!
 	allocate(psi2(sz2))
 	psi2 = 1.d0
 	call assign_psi(psi2)
-	write(*,'(3(ES12.4,1X))') rho0
+	!write(*,'(3(ES12.4,1X))') rho0
 	!
+	call evo_npi
 end program test
