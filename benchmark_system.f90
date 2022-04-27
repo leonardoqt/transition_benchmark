161253cd4348832e850c57d0dbf6eb79179a5ef5
Tian Qiu <tianq@sas.upenn.edu>
2022-04-26 22:05:21 -0400

    coded loc01 and loc19, the result of loc19 is strange

--- benchmark_system.f90
+++ benchmark_system.f90
@@ -6,7 +6,7 @@ module benchmark_system
 	!
 	real(dp)  :: v0, dt
 	integer   :: nT, nstate
-	real(dp), allocatable :: U(:,:,:), E(:,:), T_rate(:,:,:)
+	real(dp), allocatable :: U(:,:,:), E(:,:)
 	real(dp), allocatable :: psi0(:), rho0(:,:) ! rho is used in real calculation
 	!
 	contains
@@ -37,12 +37,10 @@ module benchmark_system
 		nstate = nstate_in
 		nT = ceiling( (x_fin_in - x_ini_in) / (v0*dt) )
 		!
-		if ( allocated(U     ) ) deallocate(U     )
-		if ( allocated(E     ) ) deallocate(E     )
-		if ( allocated(T_rate) ) deallocate(T_rate)
-		allocate(      U(nstate,nstate,nT)   )
-		allocate(      E(nstate,nT)          )
-		allocate( T_rate(nstate,nstate,nT-1) )
+		if ( allocated(U) ) deallocate(U)
+		if ( allocated(E) ) deallocate(E)
+		allocate( U(nstate,nstate,nT) )
+		allocate( E(nstate,nT)        )
 		if ( allocated(psi0) ) deallocate(psi0)
 		if ( allocated(rho0) ) deallocate(rho0)
 		allocate( psi0(nstate) )
@@ -74,7 +72,7 @@ module benchmark_system
 		rho0(:,1) = psi0
 		rho0 = matmul( rho0(:,1:1), transpose(rho0(:,1:1)) )
 		!
-	end subroutine assign_psi
+	end subroutine
 	!
 	subroutine assign_rho(rho_in)
 		!
@@ -85,19 +83,260 @@ module benchmark_system
 		!
 		rho0 = rho_in
 		!
-	end subroutine assign_rho
+	end subroutine
 	!
 	!
-	subroutine evo_npi()
+	!
+	subroutine evo_npi(Ut, Tv)
+		! it generates Ut and Tv rate, such that Ut*rho_diabat*Ut^\dagger is rho(t)_adiabats
+		! and Tv*dt is the hopping rate
 		!
 		implicit none
 		!
-		integer :: idt
+		complex(dp), allocatable :: Ut(:,:,:)
+		real(dp)   , allocatable :: Tv(:,:,:)
+		!
+		complex(dp), allocatable :: U_dt(:,:), iHTdt(:,:)
+		integer :: idt, istate
+		!
+		!
+		if ( allocated(Ut) ) deallocate(Ut)
+		if ( allocated(Tv) ) deallocate(Tv)
+		allocate( Ut(nstate,nstate,nT  ) )
+		allocate( Tv(nstate,nstate,nT-1) )
+		!
+		allocate(  U_dt(nstate,nstate) )
+		allocate( iHTdt(nstate,nstate) )
 		!
 		do idt = 1, nT-1
-			T_rate(:,:,idt) = logm( matmul(transpose(U(:,:,idt)),U(:,:,idt+1)) ) / dt
+			Tv(:,:,idt) = logm( matmul(transpose(U(:,:,idt)),U(:,:,idt+1)) ) / dt
 		enddo
+		!
+		! evolution of rho
+		Ut(:,:,1) = transpose( U(:,:,1) )
+		do idt = 1, nT-1
+			iHTdt = (0.d0, 0.d0)
+			do istate = 1, nstate
+				iHTdt(istate,istate) = cmplx(0.d0, -(E(istate,idt)+E(istate,idt+1))/2*dt, dp)
+			enddo
+			iHTdt = iHTdt - Tv(:,:,idt)*dt
+			U_dt = expm(iHTdt)
+			Ut(:,:,idt+1) = matmul(U_dt, U(:,:,idt))
+		enddo
+		!
+		deallocate(U_dt,iHTdt)
+		!
 	end subroutine evo_npi
+	!
+	!
+	subroutine evo_hst(Ut, Tv)
+		! it generates Ut and Tv rate, such that Ut*rho_diabat*Ut^\dagger is rho(t)_adiabats
+		! and Tv*dt is the hopping rate
+		!
+		implicit none
+		!
+		complex(dp), allocatable :: Ut(:,:,:)
+		real(dp)   , allocatable :: Tv(:,:,:)
+		!
+		complex(dp), allocatable :: U_dt(:,:), iHTdt(:,:)
+		integer :: idt, istate
+		!
+		!
+		if ( allocated(Ut) ) deallocate(Ut)
+		if ( allocated(Tv) ) deallocate(Tv)
+		allocate( Ut(nstate,nstate,nT  ) )
+		allocate( Tv(nstate,nstate,nT-1) )
+		!
+		allocate(  U_dt(nstate,nstate) )
+		allocate( iHTdt(nstate,nstate) )
+		!
+		do idt = 1, nT-1
+			Tv(:,:,idt) = ( matmul(transpose(U(:,:,idt)),U(:,:,idt+1)) - matmul(transpose(U(:,:,idt+1)),U(:,:,idt)) ) / (2*dt)
+		enddo
+		!
+		! evolution of rho
+		Ut(:,:,1) = transpose( U(:,:,1) )
+		do idt = 1, nT-1
+			iHTdt = (0.d0, 0.d0)
+			do istate = 1, nstate
+				iHTdt(istate,istate) = cmplx(0.d0, -(E(istate,idt)+E(istate,idt+1))/2*dt, dp)
+			enddo
+			iHTdt = iHTdt - Tv(:,:,idt)*dt
+			U_dt = expm(iHTdt)
+			Ut(:,:,idt+1) = matmul(U_dt, U(:,:,idt))
+		enddo
+		!
+		deallocate(U_dt,iHTdt)
+		!
+	end subroutine evo_hst
+	!
+	!
+	subroutine evo_loc01(Ut, Tv)
+		! https://doi.org/10.1063/1.1376633
+		!
+		! it generates Ut and Tv rate, such that Ut*rho_diabat*Ut^\dagger is rho(t)_adiabats
+		! and Tv*dt is the hopping rate
+		!
+		implicit none
+		!
+		complex(dp), allocatable :: Ut(:,:,:)
+		real(dp)   , allocatable :: Tv(:,:,:)
+		!
+		real(dp)   , allocatable :: S(:,:),vec(:,:),ww(:,:),val(:),Hloc(:,:)
+		complex(dp), allocatable :: U_dt(:,:), iHTdt(:,:)
+		integer :: idt, istate
+		!
+		!
+		if ( allocated(Ut) ) deallocate(Ut)
+		if ( allocated(Tv) ) deallocate(Tv)
+		allocate( Ut(nstate,nstate,nT  ) )
+		allocate( Tv(nstate,nstate,nT-1) )
+		!
+		allocate(     S(nstate,nstate) )
+		allocate(    ww(nstate,nstate) )
+		allocate(   vec(nstate,nstate) )
+		allocate(   val(nstate) )
+		allocate(  Hloc(nstate,nstate) )
+		allocate(  U_dt(nstate,nstate) )
+		allocate( iHTdt(nstate,nstate) )
+		!
+		do idt = 1, nT-1
+			S = matmul(transpose(U(:,:,idt)),U(:,:,idt+1))
+			call diag_real( matmul(transpose(S),S), vec, val )
+			do istate = 1, nstate
+				ww(:,istate) = vec(:,istate) / sqrt(val(istate))
+			enddo
+			Tv(:,:,idt) = matmul( matmul(S,ww), transpose(vec) )
+		enddo
+		!
+		! evolution of rho
+		Ut(:,:,1) = transpose( U(:,:,1) )
+		do idt = 1, nT-1
+			do istate = 1, nstate
+				Hloc(:,istate) = Tv(:,istate,idt) * E(istate,idt+1)
+			enddo
+			Hloc = matmul(Hloc, transpose(Tv(:,:,idt)))
+			!
+			iHTdt = (0.d0, 0.d0)
+			do istate = 1, nstate
+				iHTdt(istate,istate) = cmplx(0.d0, -E(istate,idt+1)/2*dt, dp)
+			enddo
+			iHTdt = iHTdt - Tv(:,:,idt)*( (0.d0, 5.d-1) * dt )
+			U_dt = expm(iHTdt)
+			U_dt = matmul(transpose(Tv(:,:,idt)), U_dt)
+			Ut(:,:,idt+1) = matmul(U_dt, U(:,:,idt))
+		enddo
+		!
+		!
+		! the following uses (19) to the first order in dt
+		! TODO: the problem of (19) is how to apply to mixed state
+		do idt = 1, nT-1
+			Tv(:,:,idt) = Tv(:,:,idt) / dt
+			do istate = 1, nstate
+				Tv(istate,istate,idt) = 0.d0
+			enddo
+		enddo
+		!
+		deallocate(S,ww,vec,val,Hloc,U_dt,iHTdt)
+		!
+	end subroutine evo_loc01
+	!
+	!
+	!
+	subroutine final_rho_hop(Ut, Tv, rho_f, hop_p, state0)
+		! use given Ut, Tv and stored rho0 to calculate final rho and the 
+		! total hopping rate from state0 to all states
+		!
+		implicit none
+		!
+		complex(dp), dimension(:,:,:)  :: Ut
+		real(dp)   , dimension(:,:,:)  :: Tv
+		integer                        :: state0
+		!
+		complex(dp), allocatable       :: rho_f(:,:)
+		real(dp)   , allocatable       :: hop_p(:)
+		!
+		real(dp)                       :: drate
+		integer                        :: idt, istate
+		!
+		if ( allocated(rho_f) ) deallocate(rho_f)
+		if ( allocated(hop_p) ) deallocate(hop_p)
+		allocate( rho_f(nstate,nstate) )
+		allocate( hop_p(nstate) )
+		!
+		hop_p = 0.d0
+		do idt = 1, nT-1
+			rho_f = matmul( matmul(Ut(:,:,idt+1), rho0), transpose(conjg(Ut(:,:,idt+1))) )
+			do istate = 1, nstate
+				drate = 2 * dble( Tv(state0,istate,idt)*rho_f(istate,state0) ) / dble( rho_f(state0,state0) ) * dt
+				if ( drate > 0.d0 ) hop_p(istate) = hop_p(istate) + drate
+			enddo
+		enddo
+		!
+	end subroutine final_rho_hop
+	!
+	!
+	subroutine final_rho_hop_loc19(Ut, Tv, rho_f, hop_p, state0)
+		! https://doi.org/10.1016/j.comptc.2019.02.009
+		!
+		! use given Ut, Tv and stored rho0 to calculate final rho and the 
+		! total hopping rate from state0 to all states
+		!
+		implicit none
+		!
+		complex(dp), dimension(:,:,:)  :: Ut
+		real(dp)   , dimension(:,:,:)  :: Tv
+		integer                        :: state0
+		!
+		complex(dp), allocatable       :: rho_f(:,:)
+		real(dp)   , allocatable       :: hop_p(:)
+		!
+		complex(dp), allocatable       :: rho_last(:,:)
+		real(dp)                       :: drate, wk, Sk, dP, xkj
+		integer                        :: idt, istate
+		!
+		if ( allocated(rho_f) ) deallocate(rho_f)
+		if ( allocated(hop_p) ) deallocate(hop_p)
+		allocate( rho_f(nstate,nstate) )
+		allocate( hop_p(nstate) )
+		!
+		allocate( rho_last(nstate,nstate) )
+		!
+		hop_p = 0.d0
+		rho_last = matmul( matmul(Ut(:,:,1), rho0), transpose(conjg(Ut(:,:,1))) )
+		do idt = 1, nT-1
+			rho_f = matmul( matmul(Ut(:,:,idt+1), rho0), transpose(conjg(Ut(:,:,idt+1))) )
+			wk = dble( rho_last(state0,state0)-rho_f(state0,state0) ) / dble( rho_last(state0,state0) )
+			if (wk > 0.d0) then
+				!
+				! calculate Sk
+				Sk = 0.d0
+				do istate = 1, nstate
+					! The diagonal of Tv is set to zero, Tv*dt is the T in the reference
+					dP = dble( rho_f(istate,istate)-rho_last(istate,istate) )
+					if ( dP > 0.d0 ) then
+						! dt is not necessary here, it is included in wk
+						Sk = Sk + abs(Tv(state0,istate,idt))*sqrt(dP)
+					endif
+				enddo
+				!
+				! calculate xkj
+				do istate = 1, nstate
+					dP = dble( rho_f(istate,istate)-rho_last(istate,istate) )
+					if ( dP > 0.d0 ) then
+						xkj = abs(Tv(state0,istate,idt))*sqrt(dP) / Sk
+						hop_p(istate) = hop_p(istate) + xkj * wk
+					endif
+				enddo
+			endif
+			!
+			rho_last = rho_f
+		enddo
+		!
+		deallocate(rho_last)
+		!
+	end subroutine final_rho_hop_loc19
+	!
 	!
 	!
 	subroutine diag_real(H_in, vec, val)
@@ -125,6 +364,7 @@ module benchmark_system
 	!
 	!
 	function logm(A_in)
+		! A is real-orthonormal matrix
 		!
 		implicit none
 		!
@@ -160,15 +400,15 @@ module benchmark_system
 		incr = 1
 		do istate = 1,nstate
 			if ( abs(wi(istate)) < 1.d-10 ) then
-				vec1(:,istate) = cmplx( vr(:,istate), zeros )
+				vec1(:,istate) = cmplx( vr(:,istate), zeros, dp )
 			else
 				if (incr == 1) then
-				vec1(:,istate)   = cmplx( vr(:,istate), vr(:,istate+1) )
-				vec1(:,istate+1) = cmplx( vr(:,istate),-vr(:,istate+1) )
+				vec1(:,istate)   = cmplx( vr(:,istate), vr(:,istate+1), dp )
+				vec1(:,istate+1) = cmplx( vr(:,istate),-vr(:,istate+1), dp )
 				endif
 				incr = -incr
 			endif
-			logw(istate,istate) = log( cmplx( wr(istate), wi(istate) ) )
+			logw(istate,istate) = log( cmplx( wr(istate), wi(istate), dp ) )
 		enddo
 		!
 		vec2 = vec1
@@ -185,5 +425,41 @@ module benchmark_system
 		deallocate(wr,wi,vl,vr,work,zeros,A,vec1,vec2,logw,cwork,ipiv)
 		!
 	end function logm
+	!
+	!
+	function expm(A_in)
+		! A is anti-Hermitian
+		!
+		implicit none
+		!
+		complex(dp), dimension(:,:) :: A_in
+		complex(dp), allocatable    :: expm(:,:)
+		!
+		! working variables
+		complex(dp), allocatable    :: vec(:,:), work(:)
+		real(dp)   , allocatable    :: val(:), rwork(:)
+		integer :: lwork, err_msg, istate
+		!
+		!
+		lwork = (2*nstate-1)*3
+		!
+		allocate( expm(nstate, nstate) )
+		!
+		allocate( vec(nstate, nstate) )
+		allocate( val(nstate) )
+		allocate( work(lwork) )
+		allocate( rwork(3*nstate-2) )
+		!
+		vec = A_in * (0.d0, -1.d0)
+		call zheev('V','U',nstate,vec,nstate,val,work,lwork,rwork,err_msg)
+		!
+		do istate = 1,nstate
+			expm(:,istate) = vec(:,istate) * exp( cmplx(0.d0, val(istate), dp) )
+		enddo
+		expm = matmul( expm, transpose(conjg(vec)) )
+		!
+		deallocate(vec,val,work,rwork)
+		!
+	end function expm
 	!
 end module benchmark_system
--- main.f90
+++ main.f90
@@ -1,21 +1,32 @@
 program test
 	!
-	use benchmark_system,  only: assign_model, assign_psi, U, E, rho0, evo_npi
+	use benchmark_system,  only: assign_model, assign_psi, rho0, evo_npi, evo_hst, evo_loc01, nT, &
+	                             final_rho_hop, final_rho_hop_loc19
 	use model_H,           only: sz1, sz2, H1, H2
 	use iso_fortran_env,   only: dp=> real64
 	!
 	implicit none
 	!
-	real(dp), allocatable :: psi2(:)
+	real(dp)   , allocatable :: psi(:), Tv_npi(:,:,:), hop_p(:)
+	complex(dp), allocatable :: Ut_npi(:,:,:), rho_f(:,:)
+	integer                  :: idt
 	!
-	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d0,H1,sz1)
-	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d0,H2,sz2)
+	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d-4,H1,sz1)
+	call assign_model(1.0d0,-0.5d0,2.5d0,1.0d-4,H2,sz2)
 	!write(*,'(3(ES12.4,1X))') U
 	!
-	allocate(psi2(sz2))
-	psi2 = 1.d0
-	call assign_psi(psi2)
-	!write(*,'(3(ES12.4,1X))') rho0
+	allocate(psi(sz2))
+	psi(1) = 1.d0
+	call assign_psi(psi)
 	!
-	call evo_npi
+	call evo_loc01(Ut_npi, Tv_npi)
+	!call final_rho_hop(Ut_npi, Tv_npi, rho_f, hop_p, 1)
+	call final_rho_hop_loc19(Ut_npi, Tv_npi, rho_f, hop_p, 1)
+	!
+	write(*,'(3(ES12.4,1X))') hop_p
+	write(*,*) ''
+	write(*,*) ''
+	write(*,'(3(ES12.4,1X))')  dble(rho_f)
+	write(*,*) ''
+	write(*,'(3(ES12.4,1X))') aimag(rho_f)
 end program test
